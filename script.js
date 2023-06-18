bec71eb3c2f90c62d69bfa10b5b2626ed8398aad
bittu1943 <136315448+bittu1943@users.noreply.github.com>
2023-06-19 01:18:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+document.getElementById("button_2").onclick =  function(){
+    window.location="page2.html";//takes you to page 2 to which is green
+}
+
+
+
+function page3() {
+    window.location = "page3.html";
+}
+
+document.getElementById("button_3").onclick = page3;//takes you to page3 which is red
+
+function page4(){
+    window.location = "page4.html"; 
+}
+document.getElementById("button_4").onclick = page4//takes you to page4 which is pink
+
+
+function page5(){
+    window.location = "page5.html"
+}
+
+document.getElementById("button_5").onclick = page5;//takes you to page5 which is white
+function lastpage(){
+    
+    window.location = "https://youtu.be/Nw6p2wRsoP4";
+}
+document.getElementById("button_last").onclick = lastpage;
+
